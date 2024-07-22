75a1605258e31ddd60102b7c42585ad8b725e242
Hari <harikrish12498@gmail.com>
2024-07-22 14:14:12 +0530

    Design update

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -3,7 +3,7 @@ import { FaLinkedin } from "react-icons/fa";
 
 const Footer = () => {
   return (
-    <footer className="bg-dark text-white text-center py-3">
+    <footer className="bg-dark text-white text-center py-3 footer">
       <div className="container">
         <p className="mb-0">
           © {new Date().getFullYear()}{" "}
